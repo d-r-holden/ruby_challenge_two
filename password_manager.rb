5b2af217ca95c6228906cde53a2f04f1ed1a822e
Daniel Holden <daniel.holden18@gmail.com>
2022-12-07 14:07:35 +0000

    first commit

--- password_manager.rb
+++ password_manager.rb
@@ -1,25 +1,4 @@
 
 class PasswordManager
     
-    def initialize 
-        @passwords = {}
-    end
-
-
-    def add(service, password)
-        special_characters_to_match = /[!@$%&]/
-        if password.length>=8 && password =~ special_characters_to_match
-            return @passwords[service] = password
-        end
-    end
-    
-    def password_for(service)
-        return @passwords[service]
-    end
-
-    def services
-        return @passwords.keys
-    end
-
 end
-
